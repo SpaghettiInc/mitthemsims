ff7045a4ec5353b87692c4074abcd315361c9e69
marklar23 <maka1406@student.miun.se>
2016-10-22 16:33:24 +0200

    Fixed a timer that counts down from 30seconds the when game is started. Also sorted game start and game end. Cleaned up some in the code

--- SimsMitthemv01/public_html/js/mitthemscripts.js
+++ SimsMitthemv01/public_html/js/mitthemscripts.js
@@ -7,7 +7,10 @@
 //Main Script page for testing for MITTHEM
 //
 $(function () {
-    //Creating the IMAGES class and stores all images in a vector.
+   /**
+    * Creating the IMAGES class and stores all images in a vector.
+    * @returns {mitthemscripts_L9.IMAGES}
+    */
     function IMAGES() {
         this.images = [$('<img />',
                     {id: 'img0',
@@ -37,18 +40,29 @@ $(function () {
     }
     //Creates a new image object
     var imgObject = new IMAGES();
-    
+
     //Adds a getPic function to retrieve a picture from the class
     imgObject.getPic = function (imgNr) {
         return this.images[imgNr];
     };
-    
+
+    imgObject.points = 0;
+
+    $('#gameStarter').click(function () {
+        var deadline = Date.parse(new Date()) + 31000;
+        initializeClock('myClock', deadline);
+        startGame();
+    });
+
     //Spawns the first image
     spawnRandomImg(imgObject);
 
-    //When dropped it checks whether the ID corresponds to the correct thrash
-    //If its correct it updates points to true and then gets rid of the image,
-    //If not correct it shakes and then gets rid of picture
+    
+    /**
+     * When dropped it checks whether the ID corresponds to the correct thrash
+     * If its correct it updates points to true and then gets rid of the image,
+     * If not correct it shakes and then gets rid of picture
+     */
     $("#glasBin").droppable({
         drop: function (event, ui) {
             if (ui.draggable.attr('id') === "img0") {
@@ -160,23 +174,86 @@ $(function () {
             }
         }
     });
-    
+
     //Function that spawns the random image
     function spawnRandomImg(imgObject) {
         var rnd = (Math.floor(Math.random() * 5));
-        var ggrip = imgObject.getPic(rnd);
-        $(ggrip).appendTo($('#mydiv')).draggable().show();
+        var rndImage = imgObject.getPic(rnd);
+        $(rndImage).appendTo($('#mydiv')).draggable().show();
+    }
+
+    //Function to update the points.
+    function updatePoints2(status) {
+        
+        if (status) {
+            imgObject.points++;
+            var oldPoints = document.getElementById("points").innerHTML;
+            document.getElementById("points").innerHTML = oldPoints + "<i class=\"fa fa-check\" style=\"color:green;padding:5px;\"></i>";
+        } else {
+            var oldPoints = document.getElementById("points").innerHTML;
+            document.getElementById("points").innerHTML = oldPoints + "<i class=\"fa fa-close\" style=\"color:red;padding:5px;\"></i>";
+        }
+    }
+
+/**
+ * 
+ * @param {type} eTime
+ * @returns {mitthemscripts_L9.getTimeRemaining.mitthemscriptsAnonym$5}
+ * Returns the time remaining
+ * 
+ */
+    function getTimeRemaining(eTime) {
+        var time = eTime - Date.parse(new Date());
+        //Date.parse(eTime)
+        var seconds = Math.floor((time / 1000) % 60);
+        var minutes = Math.floor((time / 1000 / 60) % 60);
+        return {
+            'total': time,
+            'minutes': minutes,
+            'seconds': seconds
+        };
+    }
+
+/**
+ * 
+ * @param {type} id
+ * @param {type} eTime
+ * @returns {undefined}
+ * Function that initializes the timer
+ */
+    function initializeClock(id, eTime) {
+        var clock = document.getElementById(id);
+        var minutesSpan = clock.querySelector('.minutes');
+        var secondsSpan = clock.querySelector('.seconds');
+        updateClock(); // run function once at first to avoid delay
+        
+        
+        var timeinterval = setInterval(updateClock, 1000);
+        
+        //Inner function to update the clock on screen, runs every 1s
+        function updateClock() {
+            var time = getTimeRemaining(eTime);
+            minutesSpan.innerHTML = ('0' + time.minutes).slice(-2);
+            secondsSpan.innerHTML = ('0' + time.seconds).slice(-2);
+            
+            //If the time goes to zero end the game and print points to screen
+            if (time.total <= 0) { 
+                clearInterval(timeinterval);
+                //alert(imgObject.points);
+                $("#gameStart").toggle('scale', 'fast');
+                $('#meHeads').text('There ya go ' + imgObject.points + ' pointserruuuu');
+                
+            }
+        }
+    }
+    
+    //Function to start the game
+    function startGame() {
+        $('#gameStart').toggle('shake', 500);
+        $('#gameStarter').remove();
     }
 });
 
-//Function to update the points.
-function updatePoints2(status) {
-    if (status) {
-        var oldPoints = document.getElementById("points").innerHTML;
-        document.getElementById("points").innerHTML = oldPoints + "<i class=\"fa fa-check\" style=\"color:green;padding:5px;\"></i>";
-    } else {
-        var oldPoints = document.getElementById("points").innerHTML;
-        document.getElementById("points").innerHTML = oldPoints + "<i class=\"fa fa-close\" style=\"color:red;padding:5px;\"></i>";
-    }
-}
+
+
 
