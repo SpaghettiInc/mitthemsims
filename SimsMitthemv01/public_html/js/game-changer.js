d85b804ac55c383fa86b4f8a1c0c8defb054d83b
Henrik Larsson <krankujr@gmail.com>
2016-11-07 14:29:52 +0100

    cleaning up the working dir

---
+++ SimsMitthemv01/public_html/js/game-changer.js
@@ -0,0 +1,36 @@
+
+$(document).ready(function() {
+
+  $("#highscore").hide();
+  $("#game").hide();
+  $("#aside").hide();
+
+  /*  Clicking on "start game" button */
+  $("#start-game-button").click(function() {
+
+    $("#wrapper").hide();
+    $("#game").show();
+  });
+
+  /*  Clicking on "end game" button */
+  $("#end-game-button").click(function() {
+
+    $("#game").hide();
+    $("#wrapper").show();
+  });
+
+  /*  Clicking on "show highscore" button */
+  $("#show-highscore-button").click(function() {
+
+    $("#wrapper").hide();
+    $("#highscore").show();
+  });
+
+  /*  Clicking on "close highscore" button */
+  $("#close-highscore-button").click(function() {
+
+    $("#highscore").hide();
+    $("#wrapper").show();
+  });
+
+});
