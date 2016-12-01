a565adae0b9ee8e477c49a5111dbd09a7757ac70
Marklar23 <maka1406@gmail.com>
2016-12-01 16:21:15 +0100

    secured it up also made sure that you add if theres less than 25 in db

--- SimsMitthemv01/public_html/php/dbHandler.php
+++ SimsMitthemv01/public_html/php/dbHandler.php
@@ -42,34 +42,42 @@ class dbHandler {
 
         $sql = "SELECT score, name FROM highscore ORDER BY score, date, id DESC LIMIT 25,1";
         $result = mysqli_query($myConn, $sql);
-
-        if ($result->num_rows > 0) {
-
+        if ($result) {
             // output data of each row
-            while($row = $result->fetch_assoc()) {
-                //return $row['score'];
-                echo json_encode($row);
-            }
+            $row = mysqli_fetch_assoc($result);
+            //return $row['score'];
+            echo json_encode($row);
+            mysqli_free_result($result);
+        } else {
+            $lowScore['score'] = 0;
+            echo json_encode($lowScore);
         }
         
         mysqli_close($myConn);
-        return;
     }
 
     // write a post (name and score) to the database
-    public function writeHighscore($name1, $score) {
-
+    public function writeHighscore($name1, $score1) {
         // open up a connection
         $con = $this->openConnection();
 
         // escape any illegal characters from input
         $name = mysqli_real_escape_string($con, $name1);
-
+        
+        if(!is_numeric($score1)){
+            mysqli_close($con);
+            return;
+        }
+        
+        if(strlen($name > 16)){
+            $name = substr($name,0 ,16);
+        }
+        
         // we need the date of today
         $date = date("Y-m-d");
 
         // build the query string
-        $query = "INSERT INTO highscore VALUES('', '{$name}', '{$date}', {$score})";
+        $query = "INSERT INTO highscore VALUES('', '{$name}', '{$date}', {$score1})";
 
         // send the query string to the database
         mysqli_query($con, $query);
